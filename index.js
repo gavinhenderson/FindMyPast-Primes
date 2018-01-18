75786ee50ee8f0a39983ab7907391dc1a51315b0
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2018-01-18 13:31:38 +0000

    Added output types

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var CLI = require('./src/CLI.js')
 var Output = require('./src/Output.js')
 var PrimeTable = require('./src/PrimeTable.js')
 
-var n = CLI.getN()
+var n = CLI.getN(argv)
 var primeTable = PrimeTable.generatePrimeTable();
 Output.commandLine(primeTable)
--- src/CLI.js
+++ src/CLI.js
@@ -2,10 +2,29 @@
   This function returns the N given in the command line interface
   This function is resposible giving a default value and raising errors
 */
-function getN(){
-  return process.argv[2]
+function getN(argv){
+  return argv[2]
+}
+
+/*
+  This enum gives the possible output types
+*/
+var outputType = {
+  RAW: 1,
+  CSV: 2,
+  HTML: 3,
+  CLI: 4
+};
+
+/*
+  This function will read the command line input and extract the chosen output type
+  THis function is also resposible for giving errors and specifing a default output type
+*/
+function getOuptutType(argv){
+  return outputType.RAW;
 }
 
 module.exports = {
-  getN
+  getN,
+  outputType
 }
